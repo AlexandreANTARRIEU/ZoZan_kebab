946a5c48159b2d985f4baf0be17719d1b661ccae
hippo <hippolyte.waligora@yahoo.fr>
2020-06-19 22:30:40 +0200

    Suppression double main

--- Application.py
+++ Application.py
@@ -1,9 +1,20 @@
 from Controller.Membercontrolleur import MemberControlleur
 from Controller.kebabcontrolleur import KebabControlleur
+from Vue.root_frame import RootFrame
+
+
 def main():
-    kbabctrl = KebabControlleur()
-    mbctrl = MemberControlleur()
-    mbctrl.create_member("Alexandre", "Antarrieu", "antarrieu@et.esiea.fr", "mmonzboub", "mmonzboub")
+    #kbabctrl = KebabControlleur()
+    #mbctrl = MemberControlleur()
+    #mbctrl.create_member("Alexandre", "Antarrieu", "antarrieu@et.esiea.fr", "mmonzboub", "mmonzboub")
+
+    # init vue
+    root = RootFrame()
+    root.master.title("ZoZan Kebab")
+    root.show_menu()
+
+    # start
+    root.mainloop()
 
 
 if __name__ == "__main__":
