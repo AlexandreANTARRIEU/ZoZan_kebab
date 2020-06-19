5313f34942a1ee4f4a2d3e0105aa67c2de6d6d56
hippo <hippolyte.waligora@yahoo.fr>
2020-06-19 22:41:11 +0200

    Ajout modèle

---
+++ Model/member_db.py
@@ -0,0 +1,21 @@
+class Member:
+    def __init__(self):
+        return
+
+    def _addCommand(self, p_id_user, p_id_kebab):
+        return
+
+    def _delCommand(self, p_id_user, p_product):
+        return
+
+    def _getCommands(self, p_id_user):
+        return
+
+    def _addUser(self, p_id_user, p_name, p_surname, p_mail, p_hash, p_key):
+        return
+
+    def _setUser(self, p_id_user, p_name, p_surname, p_mail, p_hash, p_key):
+        return
+
+    def _delUser(self, p_id_user):
+        return
